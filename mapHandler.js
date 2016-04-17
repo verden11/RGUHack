8ffd92e7f2d24671fd538a7096c13f7a7ae2e03e
RmK9 <th3rmk@gmail.com>
2016-04-17 05:56:12 +0100

    Added radio button click handlers and map change for different json files.

--- mapHandler.js
+++ mapHandler.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+﻿$(document).ready(function() {
 	
 	var windowWidth = $(window).width();
 	var windowHeight = $(window).height();
@@ -10,33 +10,90 @@ $(document).ready(function() {
 	mapContainer.height((windowHeight * 0.9));
 	
 	//Adjust filter container height and width
-	$("#filter-container").height(windowHeight);
+	//$("#filter-container").height(windowHeight);
     $("#filter-container").width((windowWidth * 0.18));
 
-	var ka = function getCountriesAndValues() {
-		var dataArr = [];
-		dataArr[0] = ['Country', 'Life Expectancy'];
-		$.each(x.data, function(index,value){
-		  if(value.Year == 2000){
-		  dataArr.push([value.Country, value.Value]);
-			console.log(value.Year);
-			console.log(value.Country);
-		  }
+    //1.Improved sanitation facilities (% of population with access) ImprovedSanitationFacilities.json
+    //2.Access to electricity (% of population) accessToElectricity.json   
+    //3.Adult literacy rate, population 15+ years, both sexes (%) adultLiteracyRatePopulation15plus.json
+    //4.PM2.5 air pollution, mean annual exposure (micrograms per cubic meter) airPollutionPolulationExposedToLevelsExceedingWHOGuidelineValue.json
+    //5.Alternative and nuclear energy (% of total energy use) alternativeAndNuclierEnergy.json
+    //6.CO2 emissions (kg per 2005 US$ of GDP) co2emissions.json
+    //7.Forest area (sq. km) forestArea.json
+    //8.Fossil fuel energy consumption (% of total) fossilFuelEnergyConsuption.json
+    //9.Gross savings (% of GDP) grossSavings.json
+    //10.Internet users (per 100 people) internetUsers.json
+    //11.Military expenditure (% of GDP) militaryExpenditure.json
+    //12.Mortality rate, under-5 (per 1,000) mortalityRateUnder5.json
+    //13.Population, female (% of total) populationFemale.json
+    //14.Population growth (annual %) populationGrowth.json
+    //15.Population, total populationTotal.json
+    //16.Pump price for gasoline (US$ per liter) pumpPriceForGasoline.json
+    //17.Total tax rate (% of commercial profits) totalTaxRate.json
+    //18.Trade in services (% of GDP) tradeInServices.json
+    //19.Unemployment, total (% of total labor force) unemployment.json
+    //20.Urban population urbanPopulation.json
+    var filterArr = [];
+    var valueArr = [
+        'Improved sanitation facilities (% of population)',
+        'Access to electricity (% of population)',
+        'Adult literacy rate, population 15+ years, both sexes (%)',
+        'PM2.5 air pollution, mean annual exposure (micrograms per cubic meter)',
+        'Alternative and nuclear energy (% of total energy use)',
+        'CO2 emissions (kg per 2005 US$ of GDP)',
+        'Forest area (sq.km)',
+        'Fossil fuel energy consumption (% of total)',
+        'Gross savings (% of GDP)',
+        'Internet users (per 100 people)',
+        'Military expenditure (% of GDP)',
+        'Mortality rate, under-5 (per 1,000)',
+        'Population, female (% of total)',
+        'Population growth (annual %)',
+        'Total Population',
+        'Pump price for gasoline (US$ per liter)',
+        'Total tax rate (% of commercial profits)',
+        'Trade in services (% of GDP)',
+        'Unemployment, total (% of total labor force)',
+        'Urban population'
+    ];
 
-		})
-		return dataArr
-	}
-	
-	google.charts.load('current', {'packages':['geochart']});
-	google.charts.setOnLoadCallback(drawRegionsMap);
+    for (var i = 0; i < 20; i++) {
+        filterArr[i] = ['Country', valueArr[i]];
+    }
 
-	function drawRegionsMap() {
-		var data = google.visualization.arrayToDataTable(new ka());
+    google.charts.load('current', { 'packages': ['geochart'] });
 
-		var options = {};
+    var year = 2000;
 
-		var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+    $("input[name='filter']").on("click", function() {
+        var val = $(this).val(); //"v10"
+        var filterNumber = parseInt(val.substring(1), 10);
+        overlay(getCountriesAndValues(window[val], filterNumber));
+    });
 
-		chart.draw(data, options);
-	}
+    function getCountriesAndValues(a, b) {
+        var dataArr = [];
+        dataArr[0] = filterArr[b];
+        $.each(a.data, function (index, value) {
+            if (value.Year == year) {
+                dataArr.push([value.Country, value.Value]);
+            }
+
+        });
+        return dataArr;
+    }
+
+    function overlay(arr) {
+        google.charts.setOnLoadCallback(drawRegionsMap);
+
+        function drawRegionsMap() {
+            var data = google.visualization.arrayToDataTable(arr);
+
+            var options = {};
+
+            var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+            chart.draw(data, options);
+        }
+    }
 });
